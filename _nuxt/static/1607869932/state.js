7a11f305ce806fe4f77ad81bf0433ad988f536e5
Wozniaxos <kacperwozniax@gmail.com>
2020-12-13 15:36:18 +0100

    644de92

---
+++ _nuxt/static/1607869932/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgh-pages\u002F_nuxt\u002Fstatic\u002F1607869932",layout:"default",error:null,serverRendered:true,routePath:"\u002F",config:{axios:{baseURL:a,browserBaseURL:a},content:{dbHash:"36e15e01"}}}}("http:\u002F\u002Flocalhost:3000"));
